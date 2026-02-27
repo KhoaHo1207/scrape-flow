df0beb23655693a4e119622044d0f86eb3dd10e4
Thomas Ho <khoaho120703@gmail.com>
2026-02-27 15:27:06 +0700

    [server] feat: sidebar and breadscrumbheader

---
+++ web/components/BreadcrumbHeader.tsx
@@ -0,0 +1,32 @@
+"use client";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function BreadcrumbHeader() {
+  const pathname = usePathname();
+  const paths = pathname === "/" ? [""] : pathname?.split("/");
+  return (
+    <div className="flex items-center flex-start">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                  {path === "" ? "Home" : path}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
