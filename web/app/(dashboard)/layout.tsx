8d253d885fc5e6abadb9c8fe1208250b675bc073
Thomas Ho <khoaho120703@gmail.com>
2026-02-28 10:47:20 +0700

    [web] feat: add prisma

--- web/app/(dashboard)/layout.tsx
+++ web/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import BreadcrumbHeader from "@/components/BreadcrumbHeader";
 import DesktopSidebar from "@/components/Sidebar";
 import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
+import { SignedIn, SignIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
@@ -12,6 +13,9 @@ export default function layout({ children }: { children: React.ReactNode }) {
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumbHeader />
           <div className="gap-1 flex items-center">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
             <ModeToggle />
           </div>
         </header>
--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "@/components/providers/AppProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <AppProvider>{children}</AppProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      afterSignOutUrl={"/sign-in"}
+      appearance={{
+        elements: {
+          formButtonPrimary:
+            "bg-primary hover:bg-primary/90 text-sm !shadow-none",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.variable} antialiased`}>
+          <AppProvider>{children}</AppProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
